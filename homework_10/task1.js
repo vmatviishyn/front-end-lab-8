6b91853c222cfa7b31a9d7f5a798e55273289e6d
Vitalik Matviishyn <vmatviishyn@gmail.com>
2018-02-24 21:20:30 +0200

    Added homework_10

---
+++ homework_10/task1.js
@@ -0,0 +1,31 @@
+let timeOut = 0;
+function debounce(callback, time) {
+	return function() {
+		clearTimeout(timeOut);
+		timeOut = setTimeout(callback, time);
+	}
+}
+
+let iterator = 0;
+
+function increaseIteratorBy1() {
+  iterator++;
+
+  printIteratorValue();
+}
+
+function printIteratorValue() {
+  console.log('Iterator value ', iterator);
+}
+
+let increaseIterator = debounce(increaseIteratorBy1, 1000);
+
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator(); // Should print 'Iterator value 1'
